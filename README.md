b193f3c2a56b9911260b09d17f08b2665400a2ea
David <davidviaxespam@gmail.com>
2026-03-16 20:29:47 -0700

    output materials

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 For "SimCity" on the App Store
 
 Calculate what materials are needed for products like a chair
+
+Example:
+input:      1 chair, 2 plank
+
+output:     MATERIALS: 6 wood, 1 nail, 1 hammer
+            TOTAL MATERIALS: 7 wood, 3 metal
--- main.py
+++ main.py
@@ -22,3 +22,40 @@ materials_list = {
     'glue':['1 plastic', '2 flask'],
 }
 
+# Print products available to manufacture
+print("List of products: ")
+print(", ".join(materials_list))
+
+requested_products = input("Enter list of products: ")
+
+# Loop through each product, and keep track of materials
+
+materials_used = {}
+
+for product_request in requested_products.split(', '):
+    quantity = product_request.split(' ')[0]
+    product = product_request.split(' ')[1]
+
+    for i in range(1, int(quantity) + 1):
+        for material_info in materials_list[product]:
+            material_quantity = material_info.split(' ')[0]
+            material = material_info.split(' ')[1]
+
+            if material in materials_used:
+                materials_used[material] = str(int(materials_used[material]) + int(material_quantity))
+            else:
+                materials_used[material] = material_quantity
+
+# Output materials
+
+print("MATERIALS:", end=" ")
+
+i = 0
+
+for material, material_quantity in materials_used.items():
+    if i < len(materials_used) - 1:
+        print(material_quantity + " " + material, end=", ")
+    else:
+        print(material_quantity + " " + material)
+
+    i += 1
