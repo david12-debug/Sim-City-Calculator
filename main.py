c8355c1156866090da5a1edc367d6a73f1c65f49
David <davidviaxespam@gmail.com>
2026-03-16 18:42:07 -0700

    made list

---
+++ main.py
@@ -0,0 +1,24 @@
+materials_list = {
+    # Hardware
+    'hammer':['1 metal', '1 wood'],
+    'tape':['1 metal', '1 plastic'],
+    'shovel':['1 metal', '1 wood', '1 plastic'],
+    'spatula':['2 metal', '2 plastic', '2 wood'],
+
+    # Farmer's Market
+    'veggies':['2 seed'],
+    'flour':['2 seed', '2 cloth'],
+    'melon':['2 seed', '1 plant'],
+
+    # Furniture
+    'chair':['2 wood', '1 nail', '1 hammer'],
+    'table':['1 plank', '2 nail', '1 hammer'],
+
+    # Building Supplies
+    'nail':['2 metal'],
+    'plank':['2 wood'],
+    'brick':['2 rock'],
+    'cement':['2 rock', '1 flask'],
+    'glue':['1 plastic', '2 flask'],
+}
+
